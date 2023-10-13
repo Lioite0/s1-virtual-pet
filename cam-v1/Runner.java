d549ee29b913f203569f374cc7c25ba5eb6c6ade
elliottrl24 <elliottrl24@ahs.local>
2023-10-13 15:05:19 -0700

    we did it

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -3,23 +3,17 @@ import javax.swing.*;
 public class Runner {
     public Runner(){
         VirtualPet p = new VirtualPet();
-        p.exercise();
-        takeARest(1000);
-        p.feed();
-        takeARest(4000);
-        p.exercise();
-        String ans = getResponse("How was your day?");
-        System.out.println(ans);
+        // p.exercise();
+        // takeARest(1000);
+        // p.feed();
+        // takeARest(4000);
+        // p.exercise();
+        p.perform();
+   
     }
 
-    public String getResponse(String q){
-        String s = (String)JOptionPane.showInputDialog(
-            new JFrame(), q, "get a response",
-                JOptionPane.PLAIN_MESSAGE);
-            return s;
-
-    }
-
+    
+   
 
     public void takeARest (int milliseconds){
 try {
